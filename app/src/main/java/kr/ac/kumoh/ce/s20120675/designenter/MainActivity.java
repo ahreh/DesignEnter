6c64c497d37181f0e20017e9565d1b024a7c7d76
mokdo <tjdahreh@naver.com>
2017-06-07 16:11:24 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/ce/s20120675/designenter/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.ce.s20120675.designenter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
